0da743a990ef01f84cd160616a209c91438d7e3c
AndrewMelnykx <135994717+AndrewMelnykx@users.noreply.github.com>
2024-07-31 23:01:52 +0200

    Add files via upload

---
+++ day12/smashedWords.js
@@ -0,0 +1,9 @@
+function smash(words) {
+  const wordsSmashed = words.join(" ");
+
+  return wordsSmashed;
+}
+
+const result = smash(["adasd", "dasdasd", "sdasasd", "dasdads"]);
+
+console.log(result);
