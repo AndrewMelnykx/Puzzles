47636ef3452f381039dd52f2ec82726210e5f0e9
AndrewMelnykx <135994717+AndrewMelnykx@users.noreply.github.com>
2024-08-30 09:37:04 +0200

    Add files via upload

---
+++ day34/peopleCount.js
@@ -0,0 +1,14 @@
+function nbYear(p0, percent, aug, p) {
+  let years = 0;
+  let population = p0;
+
+  while (population < p) {
+    population += Math.floor((population * percent) / 100) + aug;
+    years++;
+  }
+
+  return years;
+}
+
+const result = nbYear(1500, 5, 100, 5000);
+console.log(result);
