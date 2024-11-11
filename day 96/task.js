c7a50341c46e31d3ce0b080a95dff4d27ad9d9df
Andrew Melnykx <andreymelnyk777@gmail.com>
2024-11-11 15:57:07 +0100

    7

---
+++ day 96/task.js
@@ -0,0 +1,22 @@
+var findWords = function (words) {
+  const resultArr = [];
+  const firstRow = "qwertyuiop";
+  const secondRow = "asdfghjkl";
+  const thirdRow = "zxcvbnm";
+
+  words.forEach((word) => {
+    const lowerWord = word.toLowerCase();
+
+    if (
+      [...lowerWord].every((char) => firstRow.includes(char)) ||
+      [...lowerWord].every((char) => secondRow.includes(char)) ||
+      [...lowerWord].every((char) => thirdRow.includes(char))
+    ) {
+      resultArr.push(word);
+    }
+  });
+
+  return resultArr;
+};
+const result = findWords(["Hello", "Alaska", "Dad", "Peace"]);
+console.log(result);
