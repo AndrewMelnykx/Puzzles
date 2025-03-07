726fd48ae79ba3b43004f12c7da987df1518ea52
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-03-07 21:06:20 +0100

    Next day 7.1

--- jsNewPracticing/day6/task.js
+++ jsNewPracticing/day6/task.js
@@ -1,1 +1,12 @@
-var merge = function (nums1, m, nums2, n) {};
+let nums1 = [1, 2, 3, 0, 0, 0];
+let m = 3;
+let nums2 = [2, 5, 6];
+let n = 3;
+
+var merge = function (nums1, m, nums2, n) {
+  nums1.length = m;
+  nums1.push(...nums2.slice(0, n));
+  nums1.sort((a, b) => a - b);
+};
+
+console.log(merge(nums1, m, nums2, n));
