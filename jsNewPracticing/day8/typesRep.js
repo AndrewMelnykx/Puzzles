42485ab99564e061d04a80b2ee1fd864d930ad17
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-03-08 18:23:15 +0100

    Next day 8.1

---
+++ jsNewPracticing/day8/typesRep.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = 2;
+
+console.log(a.toString(2));
+//turn it to binary
+
+// console.log(typeof (a << b));
+
+console.log(parseInt(a << b));
+//compare in binary and turn to number
+
+// a * (2 ** b);
+//how left shift operator works
