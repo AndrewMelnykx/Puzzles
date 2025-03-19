3d5a22dd9c1dda708739cd1c0c0c43c37f1aa565
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-03-19 18:51:50 +0100

    Next day 16 repeating

--- jsNewPracticing/thirdWeek/day17/secondTask.js
+++ jsNewPracticing/thirdWeek/day17/secondTask.js
@@ -40,5 +40,31 @@ var search = function (nums, target) {
   return -1; // Target not found
 };
 
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((nums[left] + nums[right]) / 2);
+    if ((nums[mid] = target)) {
+      return nums[mid];
+    }
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (nums[mid] < target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  return -1;
+};
+
 let output = search(nums, target);
 console.log(output);
--- jsNewPracticing/thirdWeek/day17/task.js
+++ jsNewPracticing/thirdWeek/day17/task.js
@@ -6,12 +6,12 @@ var findMaxAverage = function (nums, k) {
   for (let i = 0; i < k; i++) {
     sum += nums[i];
   }
-
   let maxSum = sum;
-  for (let i = 4; i < nums.length - 1; i++) {
+  for (let i = k; i < nums.length - 1; i++) {
     sum += nums[i] - nums[i - k];
     maxSum = Math.max(maxSum, sum);
   }
+  return maxSum / 4;
 };
 
 let output = findMaxAverage(nums, k);
