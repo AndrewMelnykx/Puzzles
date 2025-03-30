ade833d9f7d57469878158afaeb939ffed3bb190
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-03-30 13:17:45 +0200

    Next day  28 repeating

--- jsNewPracticing/forthWeek/day 25/secondTask.js
+++ jsNewPracticing/forthWeek/day 25/secondTask.js
@@ -1,43 +1,45 @@
-let nums = [5, 5, 1, 1, 4, 2, 7, 9];
+// let nums = [5, 5, 1, 1, 4, 2, 7, 9];
+// var jump = function (nums) {
+//   let jumps = 0;
+//   let lastEnd = 0;
+//   let farthest = 0;
 
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     farthest = Math.max(farthest, i + nums[i]);
+//     if (i === lastEnd) {
+//       jumps++;
+//       lastEnd = farthest;
+//     }
+//     if (lastEnd >= nums.length - 1) break;
+//   }
+//   return jumps;
+// };
+let nums = [2, 3, 1, 1, 4];
 var jump = function (nums) {
   let jumps = 0;
+  let lastEnd = 0;
   let farthest = 0;
-  let currentEnd = 0;
 
   for (let i = 0; i < nums.length - 1; i++) {
     farthest = Math.max(farthest, i + nums[i]);
-    if (i === currentEnd) {
+    console.log(
+      `Index: ${i}, nums[i]: ${nums[i]}, Farthest: ${farthest}, LastEnd: ${lastEnd}, Jumps: ${jumps}`
+    );
+
+    if (i === lastEnd) {
+      // Only jump when we reach lastEnd
       jumps++;
-      currentEnd = farthest;
+      lastEnd = farthest;
+      console.log(`Jumping! New LastEnd: ${lastEnd}, Total Jumps: ${jumps}`);
     }
-    if (currentEnd >= nums.length - 1) break;
+
+    if (lastEnd >= nums.length - 1) break;
   }
-  for (let i = 0; i < nums.length - 1; i++) {
-    farthest = Math.max(farthest, i + nums[i]);
-    if (i === currentEnd) {
-      jumps++;
-      currentEnd = farthest;
-    }
-  }
+
   return jumps;
 };
+
+console.log(jump([2, 3, 1, 1, 4]));
 
 let output = jump(nums);
 console.log(output);
-
-var jump = function (nums) {
-  let jumps = 0;
-  let farthest = 0;
-  let currentENd = 0;
-
-  for (let i = 0; i < nums.length - 1; i++) {
-    farthest = Math.max(farthest, i + nums[i]);
-    if (i === currentENd) {
-      jumps++;
-      currentENd = farthest;
-    }
-    if (currentENd >= nums.length - 1) break;
-  }
-  return jumps;
-};
