9dec61270b86ab193f8fb8d17fe72ed4ca9586bd
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-03-31 14:56:27 +0200

    Next day  29 repeating

--- jsNewPracticing/fifthWeek/day29/task.js
+++ jsNewPracticing/fifthWeek/day29/task.js
@@ -22,3 +22,29 @@ function checkBrackets(input) {
   }
   return stack.length === 0 ? "Success" : stack[stack.length - 1].index;
 }
+
+function checkBrackets(input) {
+  const stack = [];
+  const matchingBracket = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+  const stackLastElement = stack.length - 1;
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+    if (char === "(" || char === "[" || char === "{") {
+      stack.push({ bracket: char, index: index + 1 });
+    } else if (char === ")" || char === "]" || char === "}") {
+      if (
+        stack.length === 0 ||
+        matchingBracket[stack[stackLastElement].bracket] !== char
+      ) {
+        return i + 1;
+      } else {
+        stack.pop();
+      }
+    }
+  }
+  return stack.length === 0 ? "Success" : stack[stackLastElement].index;
+}
