a80218c2577176e1d8ef4d8c79711e9c8f8d2e47
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-04-23 20:21:29 +0200

    Next day  53

--- day 127/task.js
+++ day 127/task.js
@@ -1,7 +1,13 @@
 let string = "loveleetcode";
 let c = "e";
 
-var shortestToChar = function (s, c) {};
+var shortestToChar = function (s, c) {
+    let resArr = [];
+    for(let i = 0 ; i < s.length;i++){
+        if(s[i])
+    }
+
+};
 
 let output = shortestToChar(string, c);
 
---
+++ day 128/task.js
@@ -0,0 +1,8 @@
+let string = "loveleetcode";
+let c = "e";
+
+var shortestToChar = function (s, c) {};
+
+let output = shortestToChar(string, c);
+
+console.log(output);
