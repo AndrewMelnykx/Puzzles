901211a75421775fcf2fed150a7b7dc53c1f2e27
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-06-04 11:36:04 +0200

    Next day  91

--- day 163/task.js
+++ day 163/task.js
@@ -4,8 +4,17 @@ let emails = [
   "testemail+david@lee.tcode.com",
 ];
 
-var numUniqueEmails = function (emails) {};
+var numUniqueEmails = function (emails) {
+  let unique = new Set();
 
+  emails.forEach((email) => {
+    let [local, domain] = email.split("@");
+    local = local.split("+")[0].replace(/\./g, "");
+    unique.add(local + "@" + domain);
+  });
+
+  return unique.size;
+};
 let result = numUniqueEmails(emails);
 
 console.log(result);
