bd7da64db4f45e4aa08308d1f7bd16c98fbf10b7
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-06-07 11:40:13 +0200

    Next day  94

--- day 166/task.js
+++ day 166/task.js
@@ -1,6 +1,14 @@
-const nums = [1, 2, 3, 3];
+const nums = [2, 1, 2, 5, 3, 2];
 
-var repeatedNTimes = function (nums) {};
+var repeatedNTimes = function (nums) {
+  let repeated = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (repeated[nums[i]] !== undefined) {
+      return nums[i];
+    }
+    repeated[nums[i]] = true;
+  }
+};
 
 const result = repeatedNTimes(nums);
 console.log(result);
