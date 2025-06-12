795c2f79a24acd5f057f6ecbd43bba0d71543f2d
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-06-12 10:43:32 +0200

    Next day  98

--- day 169/task.js
+++ day 169/task.js
@@ -1,4 +1,4 @@
-// let n = 3;
+let n = 3;
 // // let trust = [
 // //   [1, 3],
 // //   [2, 3],
@@ -13,7 +13,9 @@ var findJudge = function (n, trust) {
   let trustCounts = new Array(n + 1).fill(0);
   for (let [a, b] of trust) {
     trustCounts[a]--;
+    console.log("minuses", trustCounts[a]--);
     trustCounts[b]++;
+    console.log("pluses", trustCounts[b]++);
   }
   for (let i = 0; i < n; i++) {
     if (trustCounts[i] === n - 1) {
