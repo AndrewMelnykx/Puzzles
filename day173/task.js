3218633e20fdf8f7883468d1efd6cf9cb7e644b6
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-06-18 15:12:25 +0200

    Next day  103 repeating

--- day173/task.js
+++ day173/task.js
@@ -2,35 +2,6 @@ let string = ["neet", "code", "love", "you"];
 
 class Solution {
   /**
-   * @param {string[]} strs
-   * @returns {string}
-   */
-  encode(strs) {
-    return strs.map(s => s.length + "#" + s).join("");
-  }
-
-  /**
-   * @param {string} str
-   * @returns {string[]}
-   */
-  decode(str) {
-    const res = [];
-    let i = 0;
-
-    while (i < str.length) {
-      let j = i;
-      while (str[j] !== "#") j++;
-      const len = parseInt(str.slice(i, j), 10);
-      const word = str.slice(j + 1, j + 1 + len);
-      res.push(word);
-      i = j + 1 + len;
-    }
-
-    return res;
-  }
-}
-class Solution {
-  /**
    * @param {string} str
    * @returns {string[]}
    */
@@ -52,6 +23,33 @@ class Solution {
       res.push(word);
       i = j + 1 + len;
     }
+  }
+}
+
+class Solution {
+  /**
+   * @param {string} str
+   * @returns {string[]}
+   */
+
+  encode(strs) {
+    return strs.map(word => word.length + "#" + word).join("");
+  }
+  /**
+   * @param {string} str
+   * @returns {string[]}
+   */
+  decode(strs) {
+    const res = [];
+    let i = 0;
+    while (i < strs.length) {
+      let j = i;
+      while (str[j] !== "#") j++;
+      const len = parseInt(strs.slice(i, j), 10);
+      const word = strs.slice(j + 1, j + 1 + len);
+      res.push(word);
+    }
+    return res;
   }
 }
 
