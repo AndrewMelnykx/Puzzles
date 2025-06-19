a31605fcfa6b20beabae2c7d9be0b3bcb52d9024
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-06-19 11:57:08 +0200

    Next day  104 repeating

--- day 174/task.js
+++ day 174/task.js
@@ -24,6 +24,29 @@ class Solution {
   }
 }
 
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {number[]}
+   */
+  productExceptSelf(nums) {
+    let numsLength = nums.length;
+    const res = Array(numsLength).fill(1);
+
+    let left = 1;
+    for (let i = 0; i < numsLength; i++) {
+      res[i] = left;
+      left *= nums[i];
+    }
+    let right = 1;
+    for (let i = numsLength - 1; i >= 0; i--) {
+      res[i] *= right;
+      right *= nums[i];
+    }
+    return res;
+  }
+}
+
 let solution = new Solution();
 
 let result = solution.productExceptSelf(nums);
