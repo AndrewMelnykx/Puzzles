94e0075b9be93e9d49c878830e2894fec8a6a04c
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-06-20 11:34:52 +0200

    Next day  105 repeating

--- day 176/task.js
+++ day 176/task.js
@@ -23,6 +23,31 @@ class Solution {
   }
 }
 
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {number}
+   */
+
+  longestConsecutive(nums) {
+    nums.sort((a, b) => a - b);
+    let count = 1;
+    let maxCount = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+      if (nums[i] === nums[i - 1]) {
+        continue;
+      } else if (nums[i] === nums[i - 1] + 1) {
+        count++;
+      } else {
+        maxCount = Math.max(maxCount, count);
+        count = 1;
+      }
+    }
+    return maxCount;
+  }
+}
+
 let solution = new Solution();
 
 let result = solution.longestConsecutive(nums);
--- day 177/task.js
+++ day 177/task.js
@@ -1,21 +1,53 @@
 let s = "Was it a car or a cat I saw";
 
-class Solution {
-  /**
-   * @param {string} s
-   * @return {boolean}
-   */
-  isPalindrome(s) {
-    let loweredAndSplitted = s.toLowerCase().replace(/[^a-z0-9]/g, "");
-    let splitted = loweredAndSplitted.split("");
-    let reversed = [...splitted].reverse();
+// class Solution {
+//   /**
+//    * @param {string} s
+//    * @return {boolean}
+//    */
+//   isPalindrome(s) {
+//     let loweredAndSplitted = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+//     let splitted = loweredAndSplitted.split("");
+//     let reversed = [...splitted].reverse();
 
-    for (let i = 0; i < splitted.length; i++) {
-      if (splitted[i] !== reversed[i]) {
-        return false;
-      }
-    }
-    return true;
+//     for (let i = 0; i < splitted.length; i++) {
+//       if (splitted[i] !== reversed[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
+
+// class Solution {
+//   /**
+//    * @param {string} s
+//    * @return {boolean}
+//    */
+//   isPalindrome(s) {
+//     let symbolless = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+//     let splitted = [...symbolless.split("")].reverse();
+
+//     for (let i = 0; i < s.length; i++) {
+//       if (s[i] !== splitted[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
+
+// GPT version
+
+// isPalindrome(s) {
+//   let cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+//   return cleaned === [...cleaned].reverse().join("");
+// }
+
+class Solution {
+  isPalindrome(s) {
+    let cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return cleaned === [...cleaned].reverse().join("");
   }
 }
 
