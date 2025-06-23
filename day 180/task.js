fb7fad438fcd2725ec311e6b0449cd38a9e923eb
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-06-23 08:44:30 +0200

    Next day  108 repeating

--- day 180/task.js
+++ day 180/task.js
@@ -25,33 +25,58 @@ class Solution {
   }
 }
 
-// class Solution {
-//   /**
-//    * @param {number[]} heights
-//    * @return {number}
-//    */
-//   maxArea(heights) {
-//     let left = 0;
-//     let right = heights.length - 1;
-//     let maxProduct = 0;
+class Solution {
+  /**
+   * @param {number[]} heights
+   * @return {number}
+   */
+  maxArea(heights) {
+    let left = 0;
+    let right = heights.length - 1;
+    let maxProduct = 0;
 
-//     while (left < right) {
-//       const heightMin = Math.min(heights[left], heights[right]);
-//       const width = right - left;
-//       const area = heightMin * width;
-//       maxProduct = Math.max(maxProduct, area);
+    while (left < right) {
+      const heightMin = Math.min(heights[left], heights[right]);
+      const width = right - left;
+      const area = heightMin * width;
+      maxProduct = Math.max(maxProduct, area);
 
-//       // Move the pointer pointing to the shorter height
-//       if (heights[left] < heights[right]) {
-//         left++;
-//       } else {
-//         right--;
-//       }
-//     }
+      // Move the pointer pointing to the shorter height
+      if (heights[left] < heights[right]) {
+        left++;
+      } else {
+        right--;
+      }
+    }
 
-//     return maxProduct;
-//   }
-// }
+    return maxProduct;
+  }
+}
+
+class Solution {
+  /**
+   * @param {number[]} heights
+   * @return {number}
+   */
+
+  maxArea(heights) {
+    let left = 0;
+    let right = heights.length - 1;
+    let maxProduct = 0;
+
+    while (left < right) {
+      const heightMin = Math.min(heights[left], heights[right]);
+      const width = right - left;
+      maxProduct = Math.max(maxProduct, area);
+    }
+    if (heights[left] < heights[right]) {
+      left++;
+    } else {
+      right--;
+    }
+    return maxProduct;
+  }
+}
 
 let solution = new Solution();
 let result = solution.maxArea(height);
