b16e9fb4e2b3376b32ff559b4b27643a4253cf06
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-06-29 17:03:23 +0200

    Next day  187 repeating

--- day 186/task.js
+++ day 186/task.js
@@ -25,6 +25,29 @@ class Solution {
     return result;
   }
 }
+class Solution {
+  /**
+   * @param {number} n
+   * @return {string[]}
+   */
+  generateParenthesis(n) {
+    const result = [];
+    function backtrack(current, open, close) {
+      if (current.length === n * 2) {
+        result.push(current);
+        return;
+      }
+      if (open < n) {
+        backtrack(+"(", open + 1, close);
+      }
+      if (close < open) {
+        backtrack(current + ")", open, close + 1);
+      }
+    }
+    backtrack("", 0, 0);
+    return result;
+  }
+}
 
 // So it equlas 3 and condtion make 3 parenthasis closed , and recursion works the way if it longer than 3 then stop because it wont succeed
 
