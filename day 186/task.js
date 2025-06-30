8a4c6fa0817c450e078a57c6774f4d817905d0ef
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-06-30 09:58:09 +0200

    Next day  187 repeating 2

--- day 186/task.js
+++ day 186/task.js
@@ -7,38 +7,16 @@ class Solution {
    * @param {number} n
    * @return {string[]}
    */
+
   generateParenthesis(n) {
     const result = [];
     function backtrack(current, open, close) {
-      if (current.length === n * 2) {
+      if (current.length == n * 2) {
         result.push(current);
         return;
       }
       if (open < n) {
-        backtrack(+"(", open + 1, close);
-      }
-      if (close < open) {
-        backtrack(current + ")", open, close + 1);
-      }
-    }
-    backtrack("", 0, 0);
-    return result;
-  }
-}
-class Solution {
-  /**
-   * @param {number} n
-   * @return {string[]}
-   */
-  generateParenthesis(n) {
-    const result = [];
-    function backtrack(current, open, close) {
-      if (current.length === n * 2) {
-        result.push(current);
-        return;
-      }
-      if (open < n) {
-        backtrack(+"(", open + 1, close);
+        backtrack(current + "(", open + 1, close);
       }
       if (close < open) {
         backtrack(current + ")", open, close + 1);
--- day 187/task.js
+++ day 187/task.js
@@ -7,8 +7,30 @@ class Solution {
    */
   dailyTemperatures(temperatures) {
     let result = [];
-    let count = 0;
+    for (let i = 0; i < temperatures.length; i++) {
+      let found = false;
+      for (let j = i + 1; j < temperatures.length; j++) {
+        if (temperatures[i] < temperatures[j]) {
+          result.push(j - i);
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        result.push(0);
+      }
+    }
+    return result;
+  }
+}
 
+class Solution {
+  /**
+   * @param {number[]} temperatures
+   * @return {number[]}
+   */
+  dailyTemperatures(temperatures) {
+    let result = [];
     for (let i = 0; i < temperatures.length; i++) {
       let found = false;
       for (let j = i + 1; j < temperatures.length; j++) {
