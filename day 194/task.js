422d2d7d2ee95f2aa04557aac16589e2402abe8b
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-08-11 11:31:12 +0200

    day 224 repeating

--- day 194/task.js
+++ day 194/task.js
@@ -42,6 +42,44 @@ class TimeMap {
   }
 }
 
+class TimeMap {
+  /**
+   * @param {string} key
+   * @param {string} value
+   * @param {number} timestamp
+   * @return {void}
+   */
+  constructor() {
+    this.keyStore = new Map();
+  }
+
+  set(key, value, timestamp) {
+    if (!this.keyStore.has(key)) {
+      this.keyStore.set(key, []);
+    }
+    this.keyStore.get(key).push([timestamp, value]);
+  }
+  get(key, timestamp) {
+    if (!this.keyStore.has(key)) {
+      return "";
+    }
+    const arr = this.keyStore.get(key);
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2);
+      if (arr[mid][0] <= timestamp) {
+        res = arr[mid][1];
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return res;
+  }
+}
+
 let timeMap = new TimeMap();
 timeMap.set("alice", "happy", 1);
 console.log(timeMap.get("alice", 1));
