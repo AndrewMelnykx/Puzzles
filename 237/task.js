d6f821ac70c51d4a5fc03457a7f7b1db017eba6e
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-11-18 20:32:19 +0100

    day 300

---
+++ 237/task.js
@@ -0,0 +1,40 @@
+let nums = [1, 2, 3, 4];
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var decompressRLElist = function (nums) {
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let n = i;
+    if (n % 2 !== 0) {
+      n + 2;
+    } else {
+      n + 1;
+    }
+    let newArr = new Array(i).fill(n);
+    result.push(...newArr);
+  }
+  result.shift();
+  result.shift();
+
+  return result;
+};
+// var decompressRLElist = function(nums) {
+//   let result = [];
+
+//   for (let i = 0; i < nums.length; i += 2) {
+//     let freq = nums[i];      // how many times to repeat
+//     let val = nums[i + 1];   // what value to repeat
+
+//     result.push(...new Array(freq).fill(val));
+//   }
+
+//   return result;
+// };
+
+let result = decompressRLElist(nums);
+
+console.log(result);
