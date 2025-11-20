99eb59427bced1e0658852e7456d366a16b884f4
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-11-20 20:12:15 +0100

    day 302

--- 238/task.js
+++ 238/task.js
@@ -17,6 +17,21 @@ var arrayRankTransform = function (arr) {
   return arr.map(x => map[x]);
 };
 
+var arrayRankTransform = function (arr) {
+  let sorted = [...arr].sort((a, b) => a - b);
+  let map = {};
+
+  let rank = 1;
+
+  for (let i = 0; i < sorted.length; i++) {
+    if (map[sorted[i]] === undefined) {
+      map[sorted[i]] = rank;
+      rank++;
+    }
+  }
+  return arr.map(x => map[x]);
+};
+
 let result = arrayRankTransform(arr);
 
 console.log(result);
