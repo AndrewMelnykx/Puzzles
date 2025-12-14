902192cf40db68c9a099da922df0768edf93fb52
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-12-14 09:08:45 +0100

    day 325

--- 253/task.js
+++ 253/task.js
@@ -1,24 +1,5 @@
 let nums = [1, 2, 3, 1];
 
-/**
- * @param {number[]} nums
- * @return {number}
- */
-// var findPeakElement = function (nums) {
-//   let peak = 0;
-//   if (nums.length <= 2 && nums[0] < nums[1]) {
-//     return 1;
-//   }
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[i] > nums[i + 1]) {
-//       peak = i;
-//     } else {
-//       continue;
-//     }
-//   }
-//   return peak;
-// };
-
 var findPeakElement = function (nums) {
   let left = 0;
   let right = nums.length - 1;
