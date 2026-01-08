034f1a0922b9bdf574127a0df63b6a39a3c4073e
Andrew Melnykx <andreymelnyk777@gmail.com>
2026-01-08 13:52:20 +0100

    351

---
+++ 268/task.js
@@ -0,0 +1,26 @@
+let nums = [10, 9, 2, 5, 3, 7, 101, 18];
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  let tails = [];
+
+  for (let num of nums) {
+    let left = 0;
+    let right = tails.length;
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2);
+      if (tails[mid] < num) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+
+    tails[left] = num;
+  }
+};
+
+let result = lengthOfLIS(nums);
